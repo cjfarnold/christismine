ec9ebc77a9fdb53c3ba7906f92c2a7742cfd51fd
Clyde Arnold <clydearnold@Clydes-MacBook-Pro.local>
2025-12-27 19:23:05 +0530

    cleaned up modal code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
 import Intro from "./components/Intro";
 import VideoGrid from "./components/VideoGrid";
-//import VideoModal from "./components/VideoModal";
 
 export default function App() {
   const [videos, setVideos] = useState([]);
-  const [selected, setSelected] = useState(null);
 
   useEffect(() => {
     fetch(`${import.meta.env.BASE_URL}data/videos.json`)
@@ -16,10 +14,7 @@ export default function App() {
   return (
     <>
       <Intro />
-      <VideoGrid videos={videos} onSelect={setSelected} />
-      {selected && (
-        <VideoModal video={selected} onClose={() => setSelected(null)} />
-      )}
+      <VideoGrid videos={videos} />
     </>
   );
 }
